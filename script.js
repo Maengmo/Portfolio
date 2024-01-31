833e2754166cf30b0b075ad9d73c97320c6d1214
MaengMo <117720344+Maengmo@users.noreply.github.com>
2024-01-31 10:57:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
+            $('.navbar').addClass("sticky"); 
         }else{
             $('.navbar').removeClass("sticky");
         }
